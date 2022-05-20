324c30bb262ed81399595469bfb6c313e7316d41
sriam <sriaman3@gmail.com>
2022-05-20 06:39:42 +0530

    new code of addition

---
+++ src/AdditonProgram.java
@@ -0,0 +1,13 @@
+
+public class AdditonProgram {
+
+	public static void main(String[] args) {
+		int a =15;
+		int b=25;
+		
+		int c=a+b;
+		//This will give you output
+		 System.out.println(c);
+	}
+
+}
